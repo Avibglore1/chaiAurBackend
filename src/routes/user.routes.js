d41bf38862a97cf3b2ed2646da01dfa4218c9a5b
Avinash <avinashkumarofficial95@gmail.com>
2025-07-19 11:01:20 +0530

    implement user account management features including password change, current user retrieval, and account updates

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import {loginUser, logoutUser, refreshAccessToken, registerUser} from "../controllers/user.controller.js";
+import {
+    changeCurrentPassword, 
+    getCurrentUser, 
+    getUserChannelProfile, 
+    getWatchHistory, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    registerUser, 
+    updateAccountDetails,
+    updateCoverImage,
+    updateUserAvatar
+} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.mw.js";
 import verifyJWT from "../middlewares/jwtAuth.js";
 
@@ -17,5 +29,16 @@ router.route('/register').post(
     ]),registerUser);
 router.route('/login').post(loginUser);
 router.route('/logout').post(verifyJWT,logoutUser);
-router.route('/refreshToken').post(refreshAccessToken)
+
+router.route('/refreshToken').post(refreshAccessToken);
+
+router.route('/changePassword').post(verifyJWT,changeCurrentPassword);
+router.route('/currentUser').get(verifyJWT,getCurrentUser);
+router.route('/updateAccount').post(verifyJWT,updateAccountDetails);
+
+router.route('/avatar').patch(verifyJWT,upload.single('avatar'),updateUserAvatar);
+router.route('/coverImage').patch(verifyJWT,upload.single('coverImage'),updateCoverImage);
+
+router.route('/channel/:username').get(verifyJWT,getUserChannelProfile);
+router.route('/history').get(verifyJWT,getWatchHistory)
 export default router
